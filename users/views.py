fa9ff2d15661d0904000efbee05a408e47fb2b6c
Muhammet Emir Aydogan <m.emr.aydogan@gmail.com>
2025-10-04 17:02:53 +0300

    feat: Add user registration API endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
